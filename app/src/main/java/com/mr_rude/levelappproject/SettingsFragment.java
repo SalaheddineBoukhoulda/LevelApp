da0bd54357ea338982c405e904ea9e2def68cfd5
RudeGames <fs_boukhoulda@esi.dz>
2018-10-27 08:32:38 +0100

    Finished a lot of layouts

--- app/src/main/java/com/mr_rude/levelappproject/SettingsFragment.java
+++ app/src/main/java/com/mr_rude/levelappproject/SettingsFragment.java
@@ -1,11 +1,15 @@
 package com.mr_rude.levelappproject;
 
+import android.app.Application;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.TextViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import net.steamcrafted.lineartimepicker.dialog.LinearTimePickerDialog;
 
 /**
  * Created by Mr_Rude on 26/10/2018.
@@ -20,6 +24,8 @@ public class SettingsFragment extends android.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.settings_fragment,container,false);
+        LinearTimePickerDialog dialog = LinearTimePickerDialog.Builder.with(getActivity()).build();
+        dialog.show();
         return rootView;
     }
 }
